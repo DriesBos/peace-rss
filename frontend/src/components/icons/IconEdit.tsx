7928b5a31f2d936fa76049a0e9be84fea5e430dd
DriesBos <info@driesbos.com>
2026-02-05 10:05:03 +0100

    updated icon

--- frontend/src/components/icons/IconEdit.tsx
+++ frontend/src/components/icons/IconEdit.tsx
@@ -1,10 +1,16 @@
 export const IconEdit = ({}) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
-      <path
-        fill="currentColor"
-        d="M11.013 1.573a1.75 1.75 0 0 1 2.475 2.474l-.793.793-2.475-2.475.793-.792zM9.513 3.073L2 10.586V13h2.414l7.513-7.513-2.414-2.414z"
-      />
+      <g>
+        <path
+          stroke="currentColor"
+          d="m10.19 4.057 1.768 1.768-8.693 8.692H1.497V12.75l8.693-8.692Z"
+        />
+        <path
+          fill="currentColor"
+          d="M12.591 1.03a.75.75 0 0 1 1.06 0l1.415 1.415a.75.75 0 0 1 0 1.06l-1.591 1.591L11 2.621l1.591-1.59Z"
+        />
+      </g>
     </svg>
   );
 };
